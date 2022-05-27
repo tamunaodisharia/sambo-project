c7b51cf477e035c62eefaf63ea23459e402b013e
nkupatadze <nkupatadze@bog.ge>
2022-05-27 15:59:35 +0400

    feat: add register Form

--- src/app/shell/content/profile/profile-actions/profile-actions.component.ts
+++ src/app/shell/content/profile/profile-actions/profile-actions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-profile-actions',
@@ -6,5 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-actions.component.scss']
 })
 
-export class ProfileActionsComponent  {
+export class ProfileActionsComponent implements OnInit  {
+
+    registerForm: any;
+    formSubmitted: boolean = false;
+
+    constructor() {
+    
+    }
+    
+    ngOnInit() {
+        this.initializeForm();
+    }
+
+    onSubmit() {
+        this.formSubmitted = true;
+
+        if (this.registerForm.invalid) return;
+    }
+
+    private initializeForm() {
+        this.registerForm = new FormGroup({
+            name: new FormControl('', Validators.required),
+            surname: new FormControl('', Validators.required),
+            description: new FormControl('')
+          });
+    }
 }
--- src/app/shell/content/profile/profile.module.ts
+++ src/app/shell/content/profile/profile.module.ts
@@ -10,11 +10,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';  
 
 @NgModule({
     declarations: [ProfileComponent, ProfileDetailsComponent, ProfileActionsComponent],
     imports: [
+        CommonModule,
+        ReactiveFormsModule,
         MatButtonModule,
         HttpClientModule,
         MatExpansionModule,
