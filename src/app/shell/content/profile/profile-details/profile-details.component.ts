b34ac1020c5c3d22117e37930042e0ddb7d78fd9
nkupatadze <nkupatadze@bog.ge>
2022-06-15 00:33:02 +0400

    feat: dynamic register service, add profile storage service

--- src/app/shell/content/profile/profile-details/profile-details.component.ts
+++ src/app/shell/content/profile/profile-details/profile-details.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProfileDtorageService } from 'src/app/shared/services/services/profile-storage.service';
 
 @Component({
   selector: 'app-profile-details',
@@ -11,7 +12,7 @@ export class ProfileDetailsComponent {
   name: string = 'ნინო';
   surname: string = 'კუპატაძე';
   position: string = 'მაგარი ვინმე';
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router, private profileDtorageService: ProfileDtorageService) {}
 
   onlogout() {
     const httpOptions = {
@@ -22,7 +23,7 @@ export class ProfileDetailsComponent {
     };
     httpOptions.headers = httpOptions.headers.set(
       'Authorization',
-      `Bearer ${localStorage.getItem('token')}`
+      `Bearer ${this.profileDtorageService.getToken()}`
     );
     return this.http
       .post(
